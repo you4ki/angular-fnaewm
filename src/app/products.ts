8132f1d794e86bd3f30af997199f1586840d6b6e
you4ki <you.4ki@gmail.com>
2022-01-29 00:57:50 +0900

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -10,22 +10,27 @@ export const products = [
     id: 1,
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
   },
   {
     id: 3,
     name: 'Phone Standard',
     price: 299,
-    description: ''
-  }
+    description: '',
+  },
+  {
+    id: 4,
+    name: 'Surface Pro',
+    price: 700.1,
+    description: 'Perfect PC',
+  },
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
